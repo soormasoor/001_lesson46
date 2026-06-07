86fe00c81e3613187a688cde77240499c62ccb03
Suren Aslanyan <suren.aslanyan.am@gmail.com>
2026-06-07 11:18:29 +0400

    Added main functionality

--- DOM_Fetch/Dog_Breeds/script.js
+++ DOM_Fetch/Dog_Breeds/script.js
@@ -2,10 +2,16 @@ const img = document.querySelector("img");
 
 const select = document.querySelector("select");
 
-select.addEventListener("change", (event) => {
-  // const data = fetch(
-  //   `https://dog.ceo/api/breed/${event.target.value}/images/random`,
-  // );
+select.addEventListener("change", async (event) => {
+  const response = await fetch(
+    `https://dog.ceo/api/breed/${event.target.value}/images/random`,
+  );
+
+  const data = await response.json();
+
+  img.src = data.message;
+
+  console.log(data);
 
   console.log(
     "Selected:::",
