01e5fc4425ea9d06085fd3d67a0bc253fdf16d66
Suren Aslanyan <suren.aslanyan.am@gmail.com>
2026-06-07 12:35:07 +0400

    Connected API, testing data structure

--- DOM_Fetch/Book_Search/script.js
+++ DOM_Fetch/Book_Search/script.js
@@ -5,11 +5,19 @@ function formatTitle(bookTitle) {
 const form = document.getElementById("searchForm");
 const inputField = document.getElementById("userInput");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const inputText = inputField.value;
   console.log("Your input:::", inputText);
+
+  const response = await fetch(
+    `http://openlibrary.org/search.json?q=${formatTitle(inputText)}`,
+  );
+
+  const data = await response.json();
+
+  console.log("Number found:::", data.numFound);
 
   inputField.value = "";
 });
