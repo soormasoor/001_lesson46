467d0018326a1541a74caf7bdcb0d830962d2faa
Suren Aslanyan <suren.aslanyan.am@gmail.com>
2026-06-07 11:56:49 +0400

    Fixed bug where sub breeds didn't get fetched

--- DOM_Fetch/Dog_Breeds/script.js
+++ DOM_Fetch/Dog_Breeds/script.js
@@ -1,10 +1,21 @@
+function separateSubbreed(breedName) {
+  const idx = breedName.indexOf("-");
+  return `${breedName.slice(0, idx)}/${breedName.slice(idx + 1)}`;
+}
+
 const img = document.querySelector("img");
 
 const select = document.querySelector("select");
 
 select.addEventListener("change", async (event) => {
+  let breed = event.target.value;
+
+  if (breed.includes("-")) {
+    breed = separateSubbreed(breed);
+  }
+
   const response = await fetch(
-    `https://dog.ceo/api/breed/${event.target.value}/images/random`,
+    `https://dog.ceo/api/breed/${breed}/images/random`,
   );
 
   const data = await response.json();
