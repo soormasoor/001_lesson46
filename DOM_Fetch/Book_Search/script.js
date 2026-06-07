62470577e2a2d589e424a72f9ec45b92ff804b87
Suren Aslanyan <suren.aslanyan.am@gmail.com>
2026-06-07 13:47:37 +0400

    Basic styling, added result count

--- DOM_Fetch/Book_Search/script.js
+++ DOM_Fetch/Book_Search/script.js
@@ -18,6 +18,14 @@ function createBookEntry(title, authorName, firstPublishYear) {
   results.appendChild(res);
 }
 
+function createCountElement(num) {
+  const res = document.createElement("h2");
+  res.id = "res-count";
+  res.textContent = `Results Found: ${num}`;
+
+  return res;
+}
+
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
@@ -35,6 +43,8 @@ form.addEventListener("submit", async (event) => {
   for (const { author_name, title, first_publish_year } of books) {
     createBookEntry(title, author_name, first_publish_year);
   }
+
+  results.before(createCountElement(data.num_found));
 
   console.log("Number found:::", data.numFound);
 
