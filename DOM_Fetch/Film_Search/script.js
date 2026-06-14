a9b432eeb529e2bc48f7a75ed54afd7920b7616e
Suren Aslanyan <suren.aslanyan.am@gmail.com>
2026-06-14 13:16:58 +0400

    Minor bug fixes

--- DOM_Fetch/Film_Search/script.js
+++ DOM_Fetch/Film_Search/script.js
@@ -17,6 +17,7 @@ async function fetchFilms() {
     const res = await fetch(API);
     if (!res.ok) throw new Error("HTTP " + res.status);
     allFilms = await res.json();
+    statusEl.textContent = `${allFilms.length} Films`;
     allFilms.sort(
       (a, b) => parseInt(a.release_date) - parseInt(b.release_date),
     );
@@ -27,19 +28,22 @@ async function fetchFilms() {
 }
 
 function render() {
-  resultsEl.innerHTML = allFilms
-    .map(
-      ({ title, description, director, release_date, image }) => `
+  resultsEl.innerHTML =
+    '<div id="results">' +
+    allFilms
+      .map(
+        ({ title, description, director, release_date, image }) => `
     <div class="film-card">
         <img class="thumbnail" src="${image}" alt="Thumbnail image for ${title}" />
         <h2 class="title">${title}</h2>
         <p class="description">${description}</p>
         <p class="director">Director: ${director}</p>
         <p class="release-date">Release date: ${release_date}</p>
-    <div>
+    </div>
     `,
-    )
-    .join("");
+      )
+      .join("") +
+    "</div>";
 }
 
 fetchFilms();
