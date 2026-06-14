7a76ece7a7996a4591d79ea47fb520072313e84b
Suren Aslanyan <suren.aslanyan.am@gmail.com>
2026-06-14 12:34:30 +0400

    Film Search - initial structure

---
+++ DOM_Fetch/Film_Search/script.js
@@ -0,0 +1,7 @@
+// DOM
+const searchEl = document.getElementById("search");
+const statusEl = document.getElementById("status");
+const resultsEl = document.getElementById("results");
+
+// Event Listeners
+searchEl.addEventListener("input", () => {});
