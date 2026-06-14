bdbaf2d27800fdd85d5b0bf0386108f6aa649817
Suren Aslanyan <suren.aslanyan.am@gmail.com>
2026-06-14 13:03:06 +0400

    Added basic page render

--- DOM_Fetch/Film_Search/script.js
+++ DOM_Fetch/Film_Search/script.js
@@ -3,5 +3,43 @@ const searchEl = document.getElementById("search");
 const statusEl = document.getElementById("status");
 const resultsEl = document.getElementById("results");
 
+// State
+const API = "https://ghibliapi.vercel.app/films";
+let allFilms = [];
+
 // Event Listeners
 searchEl.addEventListener("input", () => {});
+
+async function fetchFilms() {
+  statusEl.innerHTML = `<div class="loader">Loading films…</div>`;
+
+  try {
+    const res = await fetch(API);
+    if (!res.ok) throw new Error("HTTP " + res.status);
+    allFilms = await res.json();
+    allFilms.sort(
+      (a, b) => parseInt(a.release_date) - parseInt(b.release_date),
+    );
+    render();
+  } catch (e) {
+    statusEl.textContent = "Live API unavailable.";
+  }
+}
+
+function render() {
+  resultsEl.innerHTML = allFilms
+    .map(
+      ({ title, description, director, release_date, image }) => `
+    <div class="film-card">
+        <img class="thumbnail" src="${image}" alt="Thumbnail image for ${title}" />
+        <h2 class="title">${title}</h2>
+        <p class="description">${description}</p>
+        <p class="director">Director: ${director}</p>
+        <p class="release-date">Release date: ${release_date}</p>
+    <div>
+    `,
+    )
+    .join("");
+}
+
+fetchFilms();
