6a28467354cf0c4fbcab223f14755e6eb033fd13
Suren Aslanyan <suren.aslanyan.am@gmail.com>
2026-06-14 13:47:10 +0400

    Added dynamic search results

--- DOM_Fetch/Film_Search/script.js
+++ DOM_Fetch/Film_Search/script.js
@@ -6,9 +6,23 @@ const resultsEl = document.getElementById("results");
 // State
 const API = "https://ghibliapi.vercel.app/films";
 let allFilms = [];
+let currentQuery = "";
 
 // Event Listeners
-searchEl.addEventListener("input", () => {});
+searchEl.addEventListener("input", (event) => {
+  currentQuery = event.target.value.toLowerCase();
+
+  const films = currentQuery
+    ? allFilms.filter(
+        ({ title, director, release_date }) =>
+          title.toLowerCase().includes(currentQuery) ||
+          director.toLowerCase().includes(currentQuery) ||
+          String(release_date).includes(currentQuery),
+      )
+    : allFilms;
+
+  render(films);
+});
 
 async function fetchFilms() {
   statusEl.innerHTML = `<div class="loader">Loading films…</div>`;
@@ -21,22 +35,24 @@ async function fetchFilms() {
     allFilms.sort(
       (a, b) => parseInt(a.release_date) - parseInt(b.release_date),
     );
-    render();
+    render(allFilms);
   } catch (e) {
     statusEl.textContent = "Live API unavailable.";
   }
 }
 
-function render() {
+function render(arr) {
   resultsEl.innerHTML =
     '<div id="results">' +
-    allFilms
+    arr
       .map(
         ({ title, description, director, release_date, image }) => `
     <div class="film-card">
         <img class="thumbnail" src="${image}" alt="Thumbnail image for ${title}" />
-        <h2 class="title">${title}</h2>
-        <p class="description">${description}</p>
+        <div class="details">
+            <h2 class="title">${title}</h2>
+            <p class="description">${description}</p>
+        </div>
         <p class="director">Director: ${director}</p>
         <p class="release-date">Release date: ${release_date}</p>
     </div>
